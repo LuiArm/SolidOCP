5f2fc256368522e71b1307867edc838c910c3019
LuiArm <Luisarmendariz24@icloud.com>
2023-08-01 22:57:23 -0600

    Initial Commit

---
+++ SolidOCP/SolidOCPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidOCPApp.swift
+//  SolidOCP
+//
+//  Created by luis armendariz on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SolidOCPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
